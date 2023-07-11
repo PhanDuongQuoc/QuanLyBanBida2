506ef77dfd834faac68ed9a324dc897ea6c6c3bc
Phú Trương Ngọc <truongngocphu12@gmail.com>
2023-07-11 23:41:08 +0700

    cái cc

---
+++ QuanLyBanBida/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanBida
+{
+    internal class Class1
+    {
+        public static void main()
+        {
+            Console.WriteLine("Hello mấy ní");
+        }
+    }
+}
